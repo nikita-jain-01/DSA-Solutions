cbf78d0aa782d948305c8e75aa16598cc5e298b9
Nikita Jain <email>
2025-04-15 00:27:08 +0530

    Missing Number In An Array

---
+++ src/main/java/dsa/preparation/array/arraystriver/MissingNumberInArray.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/missing-number/
+package dsa.preparation.array.arraystriver;
+
+import java.util.Arrays;
+
+public class MissingNumberInArray {
+    public int missingNumber(int[] nums) {
+        int result=-1;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]!=i) {
+                result=i;
+                break;
+            }
+        }
+        if(result==-1)
+            return nums.length;
+        return result;
+    }
+}
